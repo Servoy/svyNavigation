4d203ad9925e53fe9c94912952b641d5d36ce7b3
Paolo <paronne@servoy.com>
2019-12-06 12:37:16 +0200

    Version form in module

---
+++ svyNavigation/forms/svyNavigationVersion.js
@@ -0,0 +1,15 @@
+/**
+ * @override
+ * @properties={typeid:24,uuid:"FA98AD3E-AB4E-4415-8FD9-6CEAB84B7DB7"}
+ */
+function getId() {
+	return 'com.servoy.bap.navigation';
+}
+
+/**
+ * @override
+ * @properties={typeid:24,uuid:"55B62A5A-A3D6-4B09-AA72-E763D83A2F58"}
+ */
+function getVersion() {
+	return '1.0.0';
+}
---
+++ svyNavigationUX/forms/svyNavigationUXVersion.js
@@ -0,0 +1,15 @@
+/**
+ * @override
+ * @properties={typeid:24,uuid:"F5FF8EDB-06C2-4559-87AF-A7B6CBDF0524"}
+ */
+function getId() {
+	return 'com.servoy.bap.navigation.ux';
+}
+
+/**
+ * @override
+ * @properties={typeid:24,uuid:"3F56B20F-BB2F-43BC-95FF-5AD5560AD751"}
+ */
+function getVersion() {
+	return '1.0.0';
+}
